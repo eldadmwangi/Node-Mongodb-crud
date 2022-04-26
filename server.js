6ad6aff6775c73ca9b4c55f18c6963e72cbaf293
eldadmwangi <eldadmwangi@gmail.com>
2022-04-27 01:42:26 +0300

    configure ejs templates and connected app to Mongodb

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
+const connectDB = require("./server/database/connection");
 
 dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 8080;
 
 //log request on console with morgan
 app.use(morgan("tiny"));
+
+//mongodb connection//
+connectDB();
 
 //parse requests to body-parser
 app.use(bodyparser.urlencoded({ extended: true }));
@@ -23,9 +27,8 @@ app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+//  catch the routes here
+app.use("/", require("./server/routes/router"));
 
 app.listen(3000, () => {
   console.log(`server is running on ${PORT}`);
