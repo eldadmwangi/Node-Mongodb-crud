2c6c8894db6abf82b2c7ebe07822cda3552f1b06
eldadmwangi <eldadmwangi@gmail.com>
2022-04-27 14:33:54 +0300

    fetch and display items from the database using axios on render

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,15 @@
+const axios = require("axios");
+
 exports.homeRoutes = (req, res) => {
-  res.render("index");
+  axios
+    .get("http://localhost:3000/api/users")
+    .then((response) => {
+      console.log(response.data);
+      res.render("index", { users: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 exports.add_user = (req, res) => {
